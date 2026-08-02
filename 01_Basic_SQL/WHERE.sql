8d5f11134b47f50626966af22feabdb931941393
Chandu D <chandudeeti47@gmail.com>
2026-08-02 19:35:02 +0530

    Update WHERE.sql

--- 01_Basic_SQL/WHERE.sql
+++ 01_Basic_SQL/WHERE.sql
@@ -1,1 +1,14 @@
 
+/*
+====================================================
+WHERE Clause
+====================================================
+
+Problem:
+Retrieve employees whose salary is greater than 50000.
+
+*/
+
+SELECT *
+FROM Employee
+WHERE Salary > 50000;
