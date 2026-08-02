b159c1a32b12f1603aa8f491781d3828938bfabd
Chandu D <chandudeeti47@gmail.com>
2026-08-02 20:14:37 +0530

    Create TEMP_TABLE_VS_TABLE_VARIABLE.sql

---
+++ 12_Table_Variables/TEMP_TABLE_VS_TABLE_VARIABLE.sql
@@ -0,0 +1,22 @@
+/*
+====================================================
+TEMP TABLE VS TABLE VARIABLE
+====================================================
+
+Comparison
+
+Temp Table
+-----------
+Stored in tempdb
+Supports indexes
+Better for large datasets
+Maintains statistics
+
+Table Variable
+--------------
+Stored in tempdb
+Limited indexing
+Best for small datasets
+No statistics
+
+*/
