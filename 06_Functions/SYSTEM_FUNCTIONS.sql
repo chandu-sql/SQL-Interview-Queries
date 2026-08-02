bb6d15ea9fa4ef23339a3d14c42da080c909e1e5
Chandu D <chandudeeti47@gmail.com>
2026-08-02 19:58:54 +0530

    Create SYSTEM_FUNCTIONS.sql

---
+++ 06_Functions/SYSTEM_FUNCTIONS.sql
@@ -0,0 +1,18 @@
+/*
+====================================================
+SQL Server System Functions
+====================================================
+
+*/
+
+SELECT GETDATE() AS CurrentDate;
+
+SELECT NEWID() AS UniqueID;
+
+SELECT LEN('SQL Server') AS StringLength;
+
+SELECT UPPER('data engineer') AS UpperCase;
+
+SELECT LOWER('SQL SERVER') AS LowerCase;
+
+SELECT ISNULL(NULL,'No Value') AS Result;
