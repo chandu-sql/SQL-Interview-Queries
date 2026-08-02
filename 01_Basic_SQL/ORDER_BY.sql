9d6c0188480cfa49532978268a9b8bb4045f627e
Chandu D <chandudeeti47@gmail.com>
2026-08-02 19:35:22 +0530

    Create ORDER_BY.sql for employee salary query
    Add SQL query to retrieve employees ordered by salary.

---
+++ 01_Basic_SQL/ORDER_BY.sql
@@ -0,0 +1,13 @@
+/*
+====================================================
+ORDER BY
+====================================================
+
+Problem:
+Retrieve employees ordered by salary in descending order.
+
+*/
+
+SELECT *
+FROM Employee
+ORDER BY Salary DESC;
