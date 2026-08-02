435563b85b9aa11c6d4b88b402d5d98944674139
Chandu D <chandudeeti47@gmail.com>
2026-08-02 20:17:08 +0530

    Create SCD_TYPE2.sql

---
+++ 13_MERGE/SCD_TYPE2.sql
@@ -0,0 +1,40 @@
+/*
+====================================================
+SCD TYPE 2
+====================================================
+
+Problem:
+Maintain history.
+
+*/
+
+-- Example only
+
+-- Existing Record
+
+EmployeeID
+
+EmployeeName
+
+Salary
+
+StartDate
+
+EndDate
+
+IsCurrent
+
+-- New record inserted
+
+-- Old record expired
+
+/*
+Typical Steps
+
+1 Update old record
+
+2 Insert new record
+
+3 Maintain history
+
+*/
